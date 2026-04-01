27ba634fce92255eb00307d157f4410deffa70c1
Anfas <anfas010101@gmail.com>
2026-04-01 09:58:40 +0530

    Database url changed

--- app/core/database.py
+++ app/core/database.py
@@ -7,7 +7,7 @@ import dotenv
 dotenv.load_dotenv()
 
 
-DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
